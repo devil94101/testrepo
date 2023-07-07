8f42b0b32d3ebe8e02a3cc08978e86ab5b01e04c
deeZoyd <deepak.paliwal@zyod.com>
2023-07-08 02:23:20 +0530

    hdcdhg

--- testpup.js
+++ testpup.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/firefox', // Path to the Firefox executable
+    executablePath: '/usr/bin/chromium', // Path to the Firefox executable
     headless: true, // Set to false if you want to see the browser UI
     args: ['--no-sandbox'] // Required for running as root user
   });
